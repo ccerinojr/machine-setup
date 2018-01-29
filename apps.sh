c411ffd944986846886188dbb9dc9ab1ce3c6cb6
Carmen Cerino <carmen.cerino@ticketmaster.com>
2018-01-29 15:16:53 -0500

    Add a script for opening a bunch of browser tabs to download apps

---
+++ apps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Textmate
+open https://github.com/textmate/textmate/releases/
+
+#Dropbox
+open https://www.dropbox.com/install
+
+#Visual Studio Code
+open https://code.visualstudio.com/download
+
+#Chrome
+open https://www.google.com/chrome/
+
+#iTerm
+open https://www.iterm2.com/downloads.html
+
+#Pixie
+open https://www.dropbox.com/s/iadwwi3rn759x1m/Pixie.app.zip?dl=0
+
+#Postman
+open https://www.getpostman.com
+
+# Snagit 4
+open https://myaccount.techsmith.com/MyProducts/Software
+
+#SourceTree
+open https://www.sourcetreeapp.com
+
+#Screensconnect 
+open https://edovia.com/screens-connect/
