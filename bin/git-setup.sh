b9feb0f558fac290a4a44a3a08d26184e0c4a59a
Carmen Cerino <ccerinojr@figma.com>
2021-09-14 10:18:58 -0400

    Update git-setup script
    - Remove work setup script
    - Add command line argument for sim linking vs copying gitconfig
    - Remove lingering bash setup steps

--- bin/git-setup.sh
+++ bin/git-setup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+is_for_work=false
+
+print_usage() {
+  printf "Usage: ..."
+}
+
+while getopts 'w' flag; do
+  case "${flag}" in
+    w) is_for_work=true ;;
+  esac
+done
+
+
 echo Alias gitignore
 ln -s ~/.machine-setup/dotfiles/gitignore ~/.gitignore_global
 
 echo Alias gitconfig
-ln -s ~/.machine-setup/dotfiles/gitconfig ~/.gitconfig
-
-echo Alias git-completion
-ln -s ~/.machine-setup/dotfiles/git-completion.bash ~/.git-completion.bash
-
-echo Alias bash-git-prompt
-ln -s ~/.machine-setup/bash-git-prompt ~/.bash-git-prompt
-
-echo Alias git-prompt-colors
-ln -s ~/.machine-setup/dotfiles/git-prompt-colors.sh ~/.git-prompt-colors.sh
-
-
-
-
-
-
+if $is_for_work; then
+	cp ~/.machine-setup/dotfiles/gitconfig ~/.gitconfig
+else 
+	ln -s ~/.machine-setup/dotfiles/gitconfig ~/.gitconfig
+fi
